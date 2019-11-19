0747376ca1148f3695787e5e295ba77fa37a31f9
Duy Nguyen <duy@berkeley.edu>
2019-11-19 13:50:24 -0800

    initial commit

---
+++ app/src/main/java/edu/berkekey/cs160/savor_it/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.berkekey.cs160.savor_it;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
